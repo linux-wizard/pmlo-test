09a7ebdb5a1ff1cd5f2404d2de00a883b2fc7799
FACORAT Fabrice <fabrice.facorat@agoda.com>
2021-07-05 14:24:44 +0700

    add rds

---
+++ modules/rds/outputs.tf
@@ -0,0 +1,23 @@
+output "random_password_pmlo-test-rds-passwd-prod" {
+    description = "Password used for RDS instance"
+    value       = random_password.pmlo-test-rds-passwd-prod.result
+    sensitive   = true
+}
+
+output "aws_db_instance_rds_hostname" {  
+    description = "RDS instance hostname"  
+    value       = aws_db_instance.pmlo-test-rds.address  
+    sensitive   = true
+}
+
+output "aws_db_instance_rds_port" {  
+    description = "RDS instance port"  
+    value       = aws_db_instance.pmlo-test-rds.port  
+    sensitive   = true
+}
+
+output "aws_db_instance_rds_username" {  
+    description = "RDS instance root username"  
+    value       = aws_db_instance.pmlo-test-rds.username  
+    sensitive   = true
+}
