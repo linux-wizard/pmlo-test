dc4d7806a6557c1dae1733a6c6441b4d506264bd
FACORAT Fabrice <fabrice.facorat@agoda.com>
2021-07-05 18:02:36 +0700

    add ssh username

--- locals.tf
+++ locals.tf
@@ -5,4 +5,5 @@ locals {
   s3_tf_state_path               = "state/${local.name-prefix}-terraform.tfstate"
   dynamodb_tf_state_lock         = "tf-state-${local.name-prefix}-lock"
   aws_key_pair_pmlo_ssh_key_name = "ssh-key-${local.name-prefix}"
+  ssh_user                       = "ec2-user"
 }
