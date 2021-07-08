296f542088249867688cb7e5ec438d4fc540d720
FACORAT Fabrice <fabrice.facorat@agoda.com>
2021-07-08 10:50:07 +0700

    add rds infoas as output

--- outputs.tf
+++ outputs.tf
@@ -20,6 +20,10 @@ output "app-servers-hostnames" {
   value = aws_instance.app_server.*.public_dns
 }
 
+output "rds-engine" {
+  description = "RDS server engine"
+  value       = module.rds.aws_db_instance_rds_engine
+}
 
 output "rds-endpoint" {
   description = "RDS server endpoint"
@@ -29,5 +33,24 @@ output "rds-endpoint" {
 output "rds-username" {
   description = "RDS server username"
   value       = module.rds.aws_db_instance_rds_username
+}
+
+output "rds-password" {
+  description = "RDS server username"
+  value       = module.rds.aws_db_instance_rds_password
   sensitive   = true
 }
+
+output "rds-database" {
+  description = "RDS database"
+  value       = var.rds_database
+}
+
+output "app-lb-dns" {
+  description = "App LB DNS"
+  value       = module.ecs_opencart.aws_lb_app_dns
+}
+
+// output "ecr_repository_worker_endpoint" {
+//     value = module.ecs_xwiki.ecr_repository_worker_endpoint
+// }
